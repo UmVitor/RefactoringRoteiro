ca989cb2feb3b6b426293a59fe4cd19bd2268fc7
Vitor Barreto <54125892+UmVitor@users.noreply.github.com>
2022-06-21 11:24:24 +0000

    Commit 15

--- Price.java
+++ Price.java
@@ -2,12 +2,8 @@ public abstract class Price {
   public abstract int getPriceCode();
   public abstract double getCharge(int daysRented);
 
-  public int getFrequentRenterPoints(int daysRented){
-    int frequentRenterPoints = 1; 
-      //Adicionar o trecho de código extraído.
-     if ((this.getPriceCode() == Movie.NEW_RELEASE) &&
-         daysRented > 1) frequentRenterPoints ++;
-     
-     return frequentRenterPoints;
-  }
+
+  public int getFrequentRenterPoints(int daysRented) {
+       return 1;
+   }
 }
